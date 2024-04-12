3314f13cb927fb2888486c1b728ea136c893ab78
graydonbenson <48363199+graydonbenson@users.noreply.github.com>
2024-04-12 15:03:42 -0600

    Create stress.js

---
+++ stress.js
@@ -0,0 +1,29 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '30s', target: 100 },  // Ramp-up to 100 users over 30 seconds
+    { duration: '1m', target: 1000 }, // Increase to 1000 users over 1 minute
+    { duration: '1m', target: 5000 }, // Increase to 5000 users over 1 minute
+    { duration: '1m', target: 10000 },// Increase to 10000 users over 1 minute
+    { duration: '1m', target: 10000 },// Stay at 10000 users for 30 seconds
+    { duration: '30s', target: 0 },    // Ramp-down to 0 users over 30 seconds
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<500'], // Ensure 95% of requests finish within 500ms
+  },
+};
+
+export default function () {
+  // Send a GET request to the provided URL
+  let response = http.get('https://gqfnc7ztrujstj3fyuzjdzjb2u0nqvus.lambda-url.us-east-2.on.aws');
+
+  // Check if the response is successful
+  check(response, {
+    'is status 200': (r) => r.status === 200,
+  });
+
+  // Sleep for a random amount of time between 1 and 5 seconds
+  sleep(Math.random() * 4 + 1);
+}
