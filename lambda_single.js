3655ac9df899e6e1f4e9c7f571b57655db09f46a
graydonbenson <48363199+graydonbenson@users.noreply.github.com>
2024-04-12 14:49:07 -0600

    Update lambda_single.js

--- lambda_single.js
+++ lambda_single.js
@@ -1,6 +1,15 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+export let options = {
+  stages: [
+    { duration: '5m', target: 1 },  // 1 virtual user arriving every second for 5 minutes
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<500'], // Ensure 95% of requests finish within 500ms
+  },
+};
+
 export default function () {
   // Send a GET request to the provided URL
   let response = http.get('https://gqfnc7ztrujstj3fyuzjdzjb2u0nqvus.lambda-url.us-east-2.on.aws');
@@ -9,9 +18,7 @@ export default function () {
   check(response, {
     'is status 200': (r) => r.status === 200,
   });
-
-  // You can add more checks as needed
-
-  // Sleep for 1 second between iterations
+  
+  // Sleep for 1 second
   sleep(1);
 }
